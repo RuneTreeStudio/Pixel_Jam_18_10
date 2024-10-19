53c2101f5ca6c0072cf2ef906f30fb84cae26a34
rayastorm <sakhyrayan9@gmail.com>
2024-10-20 00:06:24 +0200

    Update obstacle.gd

--- scripts/obstacle.gd
+++ scripts/obstacle.gd
@@ -19,9 +19,9 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	var obstacle = obstacle_scene.instantiate()
 
 	# Choose a random location on Path2D.
-	var liste_spawn_location = [$Point_1,$Point_2,$Point_2]
+	var liste_spawn_location = [$Point_1,$Point_2,$Point_3]
 	var obstacle_spawn_location=liste_spawn_location[randi() % liste_spawn_location.size()]
-
+	obstacle.position=obstacle_spawn_location.position
 	# Choose the velocity for the mob.
 	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
 
