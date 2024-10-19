534f1852e2451e29cc0a9175a1d16e7f2769bd80
rayastorm <sakhyrayan9@gmail.com>
2024-10-19 23:18:20 +0200

    le debut des scenes joueur, obstacle

---
+++ scripts/joueur.gd
@@ -0,0 +1,25 @@
+extends Sprite2D
+
+var speed = 400
+var screen_size
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 2
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 2
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
---
+++ scripts/obstacle.gd
@@ -0,0 +1,30 @@
+extends Sprite2D
+
+@export var speed = 400
+var screen_size
+@export var obstacle_scene: PackedScene
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO
+	velocity.y=speed
+	position += velocity * delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	var obstacle = obstacle_scene.instantiate()
+
+	# Choose a random location on Path2D.
+	var liste_spawn_location = [$Point_1,$Point_2,$Point_2]
+	var obstacle_spawn_location=liste_spawn_location[randi() % liste_spawn_location.size()]
+
+	# Choose the velocity for the mob.
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+
+	# Spawn the mob by adding it to the Main scene.
+	add_child(obstacle)
+	queue_free()
