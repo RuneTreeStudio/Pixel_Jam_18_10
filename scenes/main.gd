419e6a4ac4aa52d7aabaa405bb8a775e1ee9eeaf
rayastorm <sakhyrayan9@gmail.com>
2024-10-20 16:30:54 +0200

    j

--- scenes/main.gd
+++ scenes/main.gd
@@ -4,7 +4,6 @@ extends Node
 var score
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$runner.show()
 	$"Beer Chug".show()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
